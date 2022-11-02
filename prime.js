d5506b8b4f772e0cad801264c2c9ef60b1090644
Pankaj-bit361 <pnkjvshsht1@gmail.com>
2022-11-02 21:21:49 +0530

    Prime code fixed by me

--- prime.js
+++ prime.js
@@ -1,9 +1,18 @@
 function ValidatePrime(num){
+let count=0;
 for(var i=0;i<=num;i++){
 if(num%i==0){
+count++;
+}
+}if(count==2){
 return true;
+}else{
+return false
 }
 }
-return false;
+let code=ValidatePrime(17)
+if(ans==true){
+console.log("This is a Prime")
+}else{
+coonsole.log("No a Prime Number");
 }
-ValidatePrime(17);
